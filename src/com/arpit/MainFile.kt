1e6f0a25d1f322ac45c71b77baa1727871a8e92e
Choudhary, Arpit <AChoudhary@mast.com>
2017-05-24 15:08:59 +0530

    first change

---
+++ src/com/arpit/MainFile.kt
@@ -0,0 +1,12 @@
+package com.arpit
+
+/**
+ * Created by achoudhary on 5/24/17.
+ */
+/**
+ * This is the main function similar to that one of main function is Java.
+ * Starting point
+ */
+fun main(args: Array<String>){
+    println("This is the first hello world")
+}
---
+++ src/com/arpit/Student.kt
@@ -0,0 +1,15 @@
+package com.arpit
+
+/**
+ * Created by achoudhary on 5/24/17.
+ */
+class Student {
+
+    /**
+     * check the difference between var (mutable) and val (immutable)
+     */
+    var name: String = "" // can assign new string to name
+
+    val human: Boolean = true // can't assign new value to this variable act as a final.,  weird name :)
+
+}
